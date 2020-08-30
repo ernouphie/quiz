ae8b39e626efd35fadf70059adc435039b590dbb
Zannie <zan@zan.com>
2020-08-30 21:13:38 +0545

    Updated Questions.

--- app/src/main/java/com/zan_ryu/quizoo/Questions.java
+++ app/src/main/java/com/zan_ryu/quizoo/Questions.java
@@ -22,18 +22,18 @@ public class Questions {
             {"geography","In which continent is the world’s longest river, the Nile?","Europe","Asia","Africa","3"},
             {"geography","In which continent is the world’s longest river, the Nile?","Europe","Asia","Africa","3"},
 
-            {"history","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"history","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"history","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"history","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"history","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"history","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
+            {"history","America was discovered in","1491","1492","1493","2"},
+            {"history","Who discovered the North pole?","Robert Peary","Magellan","Captain James","1"},
+            {"history","American Revolution started in","1775","1776","1777","1"},
+            {"history","In which year Hitler became the Chancellor of Germany?","1930","1931","1932","3"},
+            {"history","In which country Aztec civilization was originated?","USA","Mexico","Egypt","2"},
+            {"history","Who is known as Father of History?","Herodotus","Homer","Mark Antony","1"},
 
-            {"music","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"music","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"music","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"music","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"music","A group of hedgehogs is known as","Pickles","Murder","Herd","1"},
-            {"music","A group of hedgehogs is known as","Pickles","Murder","Herd","1"}
+            {"music","Which pop singer’s real name is Robyn Fenty?","Rihana","Beyonce","Marilyn","1"},
+            {"music","Who won top R&B Male Artist at the Billboard Music Awards 2018?","Zayn Malik","Bruno Mars","Ed Sheeran","2"},
+            {"music","Who sang a duet with Ed Sheeran on the song “Perfect”?","Iggy Azalea","Beyonce","Selena Gomez","2"},
+            {"music","What country is Shakira originally from?","Columbia","Mexico","Spain","1"},
+            {"music","The 2017 album “DAMN.” was released by which artist?","Eminem","Snoop Dogg","Kendrick Lamar","3"},
+            {"music","What singer wrote and performed the song “Mean”?","Taylor Swift","Doja Cat","Selena Gomez","1"}
     };
 }
